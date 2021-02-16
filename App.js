f869c582f26d275c1b304d53e900c5b56446f556
akrbanjevic1 <akrbanjevic1@luc.edu>
2021-02-16 16:09:53 -0600

    added firebase connection; added log-in interface

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import firebase from 'firebase';
+import {Button, Content, Header, Form, Input, Item, Label, Container} from 'native-base';
+
+//here we establish connection to firebase.
+const firebaseConfig = {
+  apiKey: "AIzaSyB7fXfYE4BuVeWWaTNaRPlFvMKXlSkX298",
+  authDomain: "microlending-9a33b.firebaseapp.com",
+  databaseURL: "https://microlending-9a33b-default-rtdb.firebaseio.com",
+  projectId: "microlending-9a33b",
+  storageBucket: "microlending-9a33b.appspot.com",
+  messagingSenderId: "846587248834",
+  appId: "1:846587248834:web:319b97453b3bef3978bb5b"
+};
 
 const Stack = createStackNavigator();
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <Button
-      title="Welcome to the MicroLending App!"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'User' })
-      }
-    />
+    <Container styles={styles.container}>
+      <Form>
+        <Item floatingLabel>
+          <Label>E-Mail</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        </Item>
+        <Item floatingLabel>
+          <Label>Password</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry={true}
+          />
+        </Item>
+        <Button style={{marginTop: 10}}
+        full rounded success >
+          <Text>Log-In</Text>
+        </Button>
+        <Button style = {{marginTop: 10}}
+        full rounded primary >
+          <Text style={{color: 'white'}}>Sign-Up</Text>
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
@@ -23,6 +57,7 @@ const ProfileScreen = ({ route }) => {
 };
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -39,5 +74,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
 });

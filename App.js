ed1a36b7a2cb5a1c036eda91f111d86e0e4c1614
akrbanjevic1 <akrbanjevic1@luc.edu>
2021-02-28 16:31:36 -0600

    established firebase connection AND login ability

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import firebase from 'firebase';
 import {Button, Content, Header, Form, Input, Item, Label, Container} from 'native-base';
+import { render } from 'react-dom';
 
 //here we establish connection to firebase.
 const firebaseConfig = {
@@ -20,60 +21,99 @@ const firebaseConfig = {
 
 const Stack = createStackNavigator();
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <Container styles={styles.container}>
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+  
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = ({
+      email: '',
+      password: ''
+    })
+  }
+
+  signUpUser = (email, password) =>{
+    try {
+      if(this.state.password.length<6) {
+        alert("Please enter at least 6 chars")
+        return;
+      }
+      else {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+      }
+    } catch (error) {
+      console.log(error.toString())
+    }
+  }
+
+  loginUser = (email, password) => {
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+        console.log(user)
+      })
+    } catch (error) {
+      console.log(error.toString())
+    }
+  }
+
+  render() {
+    return (
+  <Container style={styles.container}>
+    <Text style={styles.header}>Welcome: Microlending</Text>
       <Form>
         <Item floatingLabel>
-          <Label>E-Mail</Label>
+          <Label style={ {color: 'white'} }>E-Mail</Label>
           <Input
             autoCorrect={false}
             autoCapitalize="none"
+            onChangeText={(email) => this.setState({email})}
           />
         </Item>
         <Item floatingLabel>
-          <Label>Password</Label>
+          <Label style={ {color: 'white'} }>Password</Label>
           <Input
             autoCorrect={false}
             autoCapitalize="none"
             secureTextEntry={true}
+            onChangeText={(password) => this.setState({password})}
           />
         </Item>
-        <Button style={{marginTop: 10}}
-        full rounded success >
-          <Text>Log-In</Text>
+        <Button style={{ marginTop: 10 }}
+        full rounded success 
+        onPress ={()=> this.loginUser(this.state.email, this.state.password)}
+        >
+          <Text style = {{ color: 'white' }}>Log-In</Text>
         </Button>
         <Button style = {{marginTop: 10}}
-        full rounded primary >
+        full rounded primary 
+        onPress = {()=> this.signUpUser(this.state.email, this.state.password)}
+        >
           <Text style={{color: 'white'}}>Sign-Up</Text>
         </Button>
       </Form>
     </Container>
-  );
-};
-
-const ProfileScreen = ({ route }) => {
-  return <Text>This is {route.params.name}'s profile (testing navigation).</Text>;
-};
-
-export default function App() {
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000000',
     justifyContent: 'center',
     padding: 10
   },
+
+  header: {
+    color: 'white',
+    fontSize: 40,
+    justifyContent: 'center',
+  }
 });

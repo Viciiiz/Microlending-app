fd302d5f987ccf14808af0cbc02d7d5f9ff3551f
mirajluc <71102547+mirajluc@users.noreply.github.com>
2021-04-23 01:21:11 -0500

    Update and rename LoansPoolsMIRAJ.js to CreateLoansPools.js

--- Screens/CreateLoansPools.js
+++ Screens/CreateLoansPools.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants'; 
 
-// or any pure javascript modules available in npm 
-import { Card } from 'react-native-paper';
+//For the manager to create loans
 
 export default function App() {
   return (
